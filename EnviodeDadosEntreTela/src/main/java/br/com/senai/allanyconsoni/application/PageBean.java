6de6955141d80044a6106d95cff0d7fd999a2ab0
Allany Consoni <102614463+aconsoni@users.noreply.github.com>
2022-04-19 17:00:19 -0300

    Add files via upload

---
+++ EnviodeDadosEntreTela/src/main/java/br/com/senai/allanyconsoni/application/PageBean.java
@@ -0,0 +1,33 @@
+package br.com.senai.allanyconsoni.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+
+public class PageBean implements Serializable {
+	private String nome;
+	
+	@Inject
+	private Flash flash;
+	
+	public String processar() {
+		flash.put("nomeDoUsuario", nome);
+		return "Result?faces-redirect=true";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
